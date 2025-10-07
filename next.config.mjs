79e9c5c3b3b10a7d357e83dba6787c8306e45ff1
Shehab Ahmed <shahbahmed56p@gmail.com>
2025-10-07 21:47:12 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: "export", // مهم للـ GitHub Pages
+  basePath: isProd ? "/shehab-portfolio-v2" : "", // ضع اسم الريبو هنا
+  assetPrefix: isProd ? "/shehab-portfolio-v2/" : "",
 }
 
-export default nextConfig
+export default nextConfig;
